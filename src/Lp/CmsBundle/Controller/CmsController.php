2af528cb49b5217fdb319a4b031885776592d076
Dmitry Sazonov <sazon@nxt.ru>
2019-02-27 18:22:09 +0300

    memcache

--- src/Lp/CmsBundle/Controller/CmsController.php
+++ src/Lp/CmsBundle/Controller/CmsController.php
@@ -4,6 +4,7 @@ namespace App\Lp\CmsBundle\Controller;
 use App\Lp\Framework\LpController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Cache\Adapter\MemcachedAdapter;
 
 class CmsController extends LpController
 {
@@ -12,6 +13,9 @@ class CmsController extends LpController
      */
     public function match()
     {
+//        $client = MemcachedAdapter::createConnection(
+//            'memcached://memcached:11211'
+//        );
         return $this->render('root/index.html.twig', [
             'controller_name' => 'CmsController',
         ]);
