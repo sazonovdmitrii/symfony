c8c76d4cab5d84f7b3717ddb62030428b3ddaa60
Dmitry Sazonov <sazon@nxt.ru>
2019-03-06 13:55:35 +0300

    service refactoring

--- src/Service/Basket.php
+++ src/Service/Basket.php
@@ -1,19 +1,28 @@
 <?php
-
 namespace App\Service;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class Basket
+class Basket extends AbstractController
 {
+    /**
+     * @return int|void
+     */
     function count()
     {
-        $request = $this->container
-            ->get('request_stack')
-            ->getCurrentRequest();
-
-        $basket = $request
+        $basket = $this->_getRequest()
             ->getSession()
             ->get('basket');
 
         return count($basket['items']->getValues());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function _getRequest()
+    {
+        return $this->container
+            ->get('request_stack')
+            ->getCurrentRequest();
     }
 }
