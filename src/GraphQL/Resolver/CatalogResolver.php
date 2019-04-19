65f50c137170e78c3d9c72cca27e647f2f9041f6
Dmitry Sazonov <sazon@nxt.ru>
2019-04-19 20:22:51 +0300

    catalog products resolver

--- src/GraphQL/Resolver/CatalogResolver.php
+++ src/GraphQL/Resolver/CatalogResolver.php
@@ -1,11 +1,15 @@
 <?php
 namespace App\GraphQL\Resolver;
 use Doctrine\ORM\EntityManager;
+use App\Entity\Catalog;
+use Doctrine\ORM\EntityManagerInterface;
+use GraphQL\Type\Definition\ResolveInfo;
 use Overblog\GraphQLBundle\Definition\Argument;
-use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+use Overblog\GraphQLBundle\Relay\Connection\Output\Connection;
+use Overblog\GraphQLBundle\Relay\Connection\Paginator;
 
-class CatalogResolver implements ResolverInterface, AliasedInterface {
+class CatalogResolver implements ResolverInterface {
 
     private $em;
 
@@ -17,6 +21,29 @@ class CatalogResolver implements ResolverInterface, AliasedInterface {
     public function __construct(EntityManager $em)
     {
         $this->em = $em;
+    }
+
+    public function __invoke(ResolveInfo $info, $value, Argument $args)
+    {
+        $method = $info->fieldName;
+        return $this->$method($value, $args);
+    }
+
+    public function name(Catalog $catalog) {
+        return $catalog->getName();
+    }
+
+    public function id(Catalog $catalog) {
+        return $catalog->getId();
+    }
+
+    public function products(Catalog $catalog, Argument $args) :Connection
+    {
+        $products = $catalog->getProducts()->toArray();
+        $paginator = new Paginator(function ($offset, $limit) use ($products) {
+            return array_slice($products, $offset, $limit ?? 10);
+        });
+        return $paginator->auto($args, count($products));
     }
 
     /**
