5182b5790b97db1603294dfafdb8a23f93a8e341
Alexey Chernyshev <er1x4ever@gmail.com>
2019-05-16 20:58:16 +0300

    search page

--- front/src/routes/Search/Search.js
+++ front/src/routes/Search/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Products from 'components/Products';
@@ -6,6 +6,15 @@ import Pagination from 'components/Pagination';
 import SearchForm from 'components/SearchForm';
 
 import SortBox from './SortBox';
+
+const MySearchForm = (
+    <div className="catalog__search">
+        Изменить запрос?
+        <div className="catalog__search-form">
+            <SearchForm />
+        </div>
+    </div>
+);
 
 const Search = ({ match, limit, count, query, rows }) => {
     const isPage = match.url.match(/(\/page-)(?<index>\d+)/);
@@ -35,38 +44,69 @@ const Search = ({ match, limit, count, query, rows }) => {
         <div className="catalogpage catalogpage-5column">
             <div className="catalogpage__content--full">
                 <h1 className="typography__catheader">Вы искали: {query}</h1>
-                <div className="catalogpage__foundcount">Мы нашли {count} товаров</div>
-                <div className="catalog__pager">
-                    {pagination}
-                    <SortBox query={query} rows={rows} />
-                    <form action="" method="GET" className="catalog-sorting" data-behavior="catalogSorting">
-                        <div className="catalog-sorting__label">Сортировать:</div>
-                        <div className="catalog-sorting__inner">
-                            <select className="catalog-sorting__select" name="sort">
-                                <option value="dateup">По дате</option>{' '}
-                                <option value="priceup">По ↑ цене</option>
-                                <option value="pricedown">По ↓ цене</option>
-                                <option value="ratingup">По рейтингу</option>
-                            </select>
-                            <i className="catalog-sorting__arrow" />
+                {count ? (
+                    <Fragment>
+                        <div className="catalogpage__foundcount">Мы нашли {count} товаров</div>
+                        <div className="catalog__pager">
+                            {pagination}
+                            <SortBox query={query} rows={rows} />
+                            <form className="catalog-sorting">
+                                <div className="catalog-sorting__label">Сортировать:</div>
+                                <div className="catalog-sorting__inner">
+                                    <select className="catalog-sorting__select" name="sort">
+                                        <option value="dateup">По дате</option>{' '}
+                                        <option value="priceup">По ↑ цене</option>
+                                        <option value="pricedown">По ↓ цене</option>
+                                        <option value="ratingup">По рейтингу</option>
+                                    </select>
+                                    <i className="catalog-sorting__arrow" />
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div>
-                <Products limit={limit} />
-                <div className="catalog__sorting catalog__sorting_pos_b">
-                    <div className="catalog__search-name">
-                        Вы искали <span className="typography__catheader">{query}</span>
-                    </div>
-                    <div className="catalog__search-counts">
-                        Мы нашли <span className="catalog__counts-i">{count} товаров</span>
-                    </div>
-                    <div className="catalog__search">
-                        Изменить запрос?
-                        <div className="catalog__search-form">
-                            <SearchForm />
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        {MySearchForm}
+                        <div
+                            style={{
+                                paddingBottom: '39px',
+                                paddingTop: '39px',
+                                paddingLeft: '20px',
+                                paddingRight: '20px',
+                                textAlign: 'center',
+                                lineHeight: '1.3125',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    fontSize: '19px',
+                                    textAlign: 'center',
+                                    fontWeight: 'bold',
+                                    marginBottom: '10px',
+                                }}
+                            >
+                                Ничего не найдено
+                            </div>
+                            <div style={{ fontSize: '15px', fontWeight: '400' }}>
+                                Поиск не принес результатов.
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </Fragment>
+                )}
+                {count ? (
+                    <Fragment>
+                        <Products limit={limit} />
+                        <div className="catalog__sorting catalog__sorting_pos_b">
+                            <div className="catalog__search-name">
+                                Вы искали <span className="typography__catheader">{query}</span>
+                            </div>
+                            <div className="catalog__search-counts">
+                                Мы нашли <span className="catalog__counts-i">{count} товаров</span>
+                            </div>
+                            {MySearchForm}
+                        </div>
+                    </Fragment>
+                ) : null}
                 {pagination}
             </div>
         </div>
