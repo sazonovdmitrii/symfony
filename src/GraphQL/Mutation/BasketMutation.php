14d52dfee6dd456872feb7cf9fee8c54a72523f3
Dmitry Sazonov <sazon@nxt.ru>
2019-06-04 20:51:45 +0300

    implement users actions mutations

--- src/GraphQL/Mutation/BasketMutation.php
+++ src/GraphQL/Mutation/BasketMutation.php
@@ -3,32 +3,42 @@ namespace App\GraphQL\Mutation;
 
 use App\GraphQL\Input\AddBasketInput;
 use App\Service\AuthenticatorService;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Overblog\GraphQLBundle\Definition\Argument;
-use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
 use Doctrine\ORM\EntityManager;
+use Redis;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class BasketMutation implements MutationInterface
+class BasketMutation extends AuthMutation
 {
-    private $jwtManager;
-
-    private $authenticator;
+    private $authenticatorService;
 
     public function __construct(
-        EntityManager $em
+        EntityManager $em,
+        Redis $redis,
+        ContainerInterface $container,
+        AuthenticatorService $authenticatorService
     ) {
+        $this->redis = $redis;
         $this->em = $em;
+        $this->authenticatorService = $authenticatorService;
+        if ($container->has('request_stack')) {
+            $this->request = $container->get('request_stack')->getCurrentRequest();
+        }
+        parent::__construct($redis, $container, $authenticatorService);
     }
 
     public function add(Argument $args)
     {
         $input = new AddBasketInput($args);
 
-        if($input->item_id) {
+        if($input->item_id && $this->user) {
+
             $productItem = $this->em
                 ->getRepository('App:ProductItem')
                 ->find($input->item_id);
-            var_dump($productItem->getId());
+            $key = 'basket' . $this->user->getId();
+            $this->redis->set($key, $productItem->getId());
+            var_dump($this->redis->get($key));
             die();
         }
     }
