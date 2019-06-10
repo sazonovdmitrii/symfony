56a716233e85d9d853d7c700ba9bb10e099d4299
Alexey Chernyshev <er1x4ever@gmail.com>
2019-06-10 18:07:39 +0300

    favicons

--- front/src/globalMeta.js
+++ front/src/globalMeta.js
@@ -1,32 +1,21 @@
 import React, { Fragment } from 'react';
 
-const data = {
+export default {
     url: `https://laparfumerie.ru`,
     domain: 'laparfumerie.ru',
     siteName: 'Laparfumerie.ru',
-    fullSiteName: 'ООО «Интернет магазин парфюмерии и косметики – Laparfumerie.ru» ',
+    fullSiteName: 'ООО «Интернет магазин парфюмерии и косметики – Laparfumerie.ru»',
     title: '',
-    work_time: '',
+    work_time: 'Работаем c 8:00 до 17:00.',
     phone: {
         moscow: '+7 (495) 539 53 15',
         russia: '8 (800) 100 53 15',
     },
-};
-
-const socials = ({ title, description, type = 'article', siteName, url, locale = 'ru_RU' }) => {
-    return (
-        <Fragment>
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:type" content={type} />
-            <meta property="og:site_name" content={data.fullSiteName} />
-            <meta property="og:url" content={`https://laparfumerie.ru${og_url}`} />
-            <meta property="og:locale" content={locale} />
-        </Fragment>
-    );
-};
-
-export default {
+    text: {
+        phone() {
+            return `Звоните ${this.phone.moscow} или ${this.phone.russia}`;
+        },
+    },
     default: {
         title: '',
     },
@@ -34,86 +23,108 @@ export default {
         description: 'Продаем элитную парфюмерию и косметику для женщин и мужчин с доставкой',
         keywords: 'парфюмерия, духи, интернет магазин парфюмерии, laparfumerie, лапарфюмерия',
     },
-    content: {
-        title: `Информационные статьи интернет магазина парфюмерии ${data.siteName}`,
-        description: `Контакты интернет магазина парфюмерии ${data.siteName}`,
-        keywords: 'контакты, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    content() {
+        return {
+            title: `Информационные статьи интернет магазина парфюмерии ${this.siteName}`,
+            description: `Контакты интернет магазина парфюмерии ${this.siteName}`,
+            keywords: 'контакты, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    brands: {
-        title: `Все бренды парфюмерии и косметики | Интернет магазин парфюмерии и косметики – ${
-            data.siteName
-        }`,
-        description: `Парфюмерия и Косметика с доставкой по Москве. Работаем ${data.work_time}. Звоните ${
-            data.phone.moscow
-        } или ${data.phone.russia}`,
-        keywords: 'парфюмерные бренды, бренды духов, интернет магазин парфюмерии, laparfumerie, лапарфюмерия',
+    brands() {
+        return {
+            title: `Все бренды парфюмерии и косметики | Интернет магазин парфюмерии и косметики – ${
+                this.siteName
+            }`,
+            description: `Парфюмерия и Косметика с доставкой по Москве. ${
+                this.work_time
+            } ${this.text.phone()}`,
+            keywords:
+                'парфюмерные бренды, бренды духов, интернет магазин парфюмерии, laparfumerie, лапарфюмерия',
+        };
     },
-    comments: {
-        title: `Отзывы о парфюмерии и косметике. Интернет магазин ${data.siteName}`,
-        description:
-            'Последние отзывы о брендовой парфюмерии и косметики. Актуальная информация о мужских и женских духах.',
-        keywords:
-            'отзывы о духах, парфюмерия отзывы, мнения о духах, мнения о парфюмерии, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    comments() {
+        return {
+            title: `Отзывы о парфюмерии и косметике. Интернет магазин ${this.siteName}`,
+            description:
+                'Последние отзывы о брендовой парфюмерии и косметики. Актуальная информация о мужских и женских духах.',
+            keywords:
+                'отзывы о духах, парфюмерия отзывы, мнения о духах, мнения о парфюмерии, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    sitemap: {
-        title: `Карта сайта | ${data.title}`,
-        description: `Парфюмерия и Косметика с доставкой по Москве. Работаем ${data.work_time}. Звоните  ${
-            data.phone.moscow
-        } или ${data.phone.russia}`,
-        keywords: 'карта сайта, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    sitemap() {
+        return {
+            title: `Карта сайта | ${this.title}`,
+            description: `Парфюмерия и Косметика с доставкой по Москве. ${
+                this.work_time
+            } ${this.text.phone()}`,
+            keywords: 'карта сайта, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    'sales-leader': {
-        title: `Лидеры продаж парфюмерии - большой каталог, низкие цены | Купить с доставкой по Москве и России в интернет магазине ${
-            data.siteName
-        }`,
-        description: `Продаем парфюмерию - лидеры продаж с доставкой по Москве и России. Работаем ${
-            data.work_time
-        }. Звоните ${data.phone.moscow} или ${data.phone.russia}`,
-        keywords:
-            'лидеры продаж парфюмерии, бренды парфюмерии лидеры продаж, лидеры продаж, духи лидеры продаж, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    'sales-leader'() {
+        return {
+            title: `Лидеры продаж парфюмерии - большой каталог, низкие цены | Купить с доставкой по Москве и России в интернет магазине ${
+                this.siteName
+            }`,
+            description: `Продаем парфюмерию - лидеры продаж с доставкой по Москве и России. ${
+                this.work_time
+            } ${this.text.phone()}`,
+            keywords:
+                'лидеры продаж парфюмерии, бренды парфюмерии лидеры продаж, лидеры продаж, духи лидеры продаж, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    new: {
-        title: `Новинки парфюмерии 2014 года. Купить с доставкой по Москве и России. Интернет магазин ${
-            data.siteName
-        }`,
-        description: `Продаем парфюмерию Новинки с доставкой по Москве. Работаем ${
-            data.work_time
-        }. Звоните  ${data.phone.moscow} или ${data.phone.russia}`,
-        keywords:
-            'новые поступления парфюмерии, новинки парфюмерии, купить новинки парфюмерии, новинки духов, купить новые духи, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    new() {
+        return {
+            title: `Новинки парфюмерии 2014 года. Купить с доставкой по Москве и России. Интернет магазин ${
+                this.siteName
+            }`,
+            description: `Продаем парфюмерию Новинки с доставкой по Москве. ${
+                this.work_time
+            } ${this.text.phone()}`,
+            keywords:
+                'новые поступления парфюмерии, новинки парфюмерии, купить новинки парфюмерии, новинки духов, купить новые духи, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    articles: {
-        title: `Информационные статьи в интернет магазине ${data.siteName}`,
-        description: `Раздел информационных статей интернет магазина парфюмерии ${data.siteName}`,
-        keywords: 'Информационная статья, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+    articles() {
+        return {
+            title: `Информационные статьи в интернет магазине ${this.siteName}`,
+            description: `Раздел информационных статей интернет магазина парфюмерии ${this.siteName}`,
+            keywords:
+                'Информационная статья, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии',
+        };
     },
-    article: {
-        title: name => `${name} информационные статьи в интернет магазине ${data.siteName}`,
-        description: name =>
-            `${name} раздел информационных статей интернет магазина парфюмерии ${data.siteName}`,
-        keywords: name =>
-            `${name} информационная статья, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии`,
+    article({ name }) {
+        return {
+            title: `${name} информационные статьи в интернет магазине ${this.siteName}`,
+            description: `${name} раздел информационных статей интернет магазина парфюмерии ${this.siteName}`,
+            keywords: `${name} информационная статья, laparfumerie, лапарфюмерия, интернет магазин духов и парфюмерии`,
+        };
     },
-    catalog: ({ name, page, minPrice }) => {
+    catalog({ name, page, minPrice }) {
         return {
             title: `${name} - купить с доставкой по Москве и России - фото, цена, отзывы в интернет-магазине ${
-                data.siteName
+                this.siteName
             }!${page > 1 ? ` Cтраница ${page}` : ''}`,
             description: `${name}${
                 minPrice ? ` по цене от ${minPrice} руб` : ''
-            } с доставкой по Москве и России. Работаем c 8:00 до 17:00. Звоните ${data.phone.moscow} или ${
-                data.phone.russia
-            }`,
-            keywords: `${name} интернет-магазин ${data.siteName}`,
+            } с доставкой по Москве и России. ${this.work_time} ${this.text.phone()}`,
+            keywords: `${name} интернет-магазин ${this.siteName}`,
         };
     },
-    product: ({ name, items = [] }) => {
+    product({ name, items = [] }) {
         return {
             title: `${name} - Купить с доставкой по Москве и России. Фото, цена, отзывы!`,
             keywords: `${items.map(({ node }) => node.name).join(', ')} купить`,
-            description: `Продаем ${name} на заказ с доставкой по Москве. Работаем c 8:00 до 17:00. Звоните ${
-                data.phone.moscow
-            } или ${data.phone.russia}`,
+            description: `Продаем ${name} на заказ с доставкой по Москве. ${
+                this.work_time
+            } ${this.text.phone()}`,
+        };
+    },
+    sale({ name }) {
+        return {
+            title: `${name} - акции парфюмерии и косметики | Интернет магазин парфюмерии и косметики - низкие цены, большой каталог, фото и отзывы. Купить духи с доставкой по Москве и России - Laparfumerie.ru`,
+            description: `${name} акции парфюмерии и косметики. Продаем парфюмерию с доставкой по Москве. ${
+                this.work_time
+            } ${this.text.phone()}`,
         };
     },
 };
