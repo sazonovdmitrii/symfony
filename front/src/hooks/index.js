f850fd3c624896b292685e9b6aa316c2fca6b7b0
Alexey Chernyshev <er1x4ever@gmail.com>
2019-07-24 15:22:25 +0300

    fix

--- front/src/hooks/index.js
+++ front/src/hooks/index.js
@@ -131,6 +131,7 @@ export const useApp = () => {
     const logout = async () => {
         hardtack.remove('token', { path: '/' });
 
+        await state.client.resetStore();
         await init();
     };
 
