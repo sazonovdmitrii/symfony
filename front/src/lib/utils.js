509c5f92be9da2acd99e2b84aa3d747c9b73ce3f
Alexey Chernyshev <er1x4ever@gmail.com>
2019-08-02 19:50:47 +0300

    redirect

---
+++ front/src/lib/utils.js
@@ -0,0 +1,16 @@
+export const clearConsole = () => {
+    process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
+};
+
+export const getPath = (original, querystring) => {
+    if (/\.\w+$/.test(original)) {
+        return null;
+    }
+    if (querystring.length) {
+        return original.slice(0, -querystring.length - 1);
+    }
+
+    return original;
+};
+
+export const missingSlash = path => path.slice(-1) !== '/';
